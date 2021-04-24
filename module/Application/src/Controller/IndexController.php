cdbeceeefd1ffb5ce0ae8119cc13526013b77d7b
Rıza Rutkay Karaca <riza.karaca@tgs.aero>
2021-04-25 00:02:02 +0300

    google api implement ve template çalışması

--- module/Application/src/Controller/IndexController.php
+++ module/Application/src/Controller/IndexController.php
@@ -12,11 +12,26 @@ namespace Application\Controller;
 
 use Laminas\Mvc\Controller\AbstractActionController;
 use Laminas\View\Model\ViewModel;
+use Google_Client;
+use Google_Service;
+use Google_Service_Translate;
 
 class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
-        return new ViewModel();
+        // $client = new Google_Client();
+        // $client->setAuthConfig(__DIR__.'/client_crd.json');
+        // $client->addScope('https://www.googleapis.com/auth/cloud-translation');
+        // $client->setAccessType('offline');
+
+        // $var=new Google_Service_Translate($client);
+
+
+
+        // $lang=$var->projects->getSupportedLanguages('projects/angular-stacker-311411/locations/global');
+        // $lang=get_object_vars($lang);
+        // var_dump($lang);
+        // return new ViewModel();
     }
 }
